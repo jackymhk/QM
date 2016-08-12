9fa15a5edf1164d968ebce9d8e1092ff8b3d6a84
jackymhk <m.jacky@gmail.com>
2016-08-12 18:19:15 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var APPSCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyhle-PX-nb9GK_a3z0Zv4hOFSP8mHeoaKe3Gws_KsFvX2pYf0/exec';
 
-var USE_PROXY = false;
+var USE_PROXY = true;
 
 var Board = 'OPFLEvqH';
 
